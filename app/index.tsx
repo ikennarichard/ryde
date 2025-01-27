37bdbc5ec8f5fda3a73177989f925134cfa94370
Ikenna Richard <oguejioforrichard@gmail.com>
2025-01-27 10:43:19 +0100

    update: check user session

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
-import { Redirect } from 'expo-router'
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect, useRouter } from "expo-router";
+import { useEffect } from "react";
 
 const Home = () => {
-  return (
-    <Redirect href="/(auth)/welcome" />
-  )
-}
+  const { isLoaded, isSignedIn } = useAuth();
+  const router = useRouter();
 
-export default Home
+  useEffect(() => {
+    if (isLoaded) {
+      if (!isSignedIn) {
+        router.replace("/(auth)/welcome");
+      }
+    }
+  }, [isLoaded, isSignedIn]);
+  
+  return <Redirect href="/(root)/(tabs)/home" />;
+};
+
+export default Home;
